f5f5540be1f35bfe14fb042c2660f36ebf8bf24a
Sakshi-21-Hub <khanvilkarsakshi.21@gmail.com>
2025-07-27 00:53:54 +0530

    Add files via upload

---
+++ includes/head.php
@@ -0,0 +1,76 @@
+<?php
+  // Include necessary database or other PHP includes here if needed
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Include necessary CSS files -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&family=Rubik:ital,wght@0,400;0,500;0,700;0,800;0,900;1,600&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  
+  <!-- Include necessary JavaScript files -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXO+5PIq3H2xu9szNWRJkqfjj5ib9VI9z3hE" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+Wy5q8L4TIc5h9A/K44U/eREWSUc8n+kW4W" crossorigin="anonymous"></script>
+
+  <style>
+    /* Custom CSS for blue strip */
+    .navbar-blue-strip {
+      background-color: navy;
+    }
+
+    /* Custom CSS for horizontal navigation links and social media icons */
+    .navbar-horizontal-links,
+    .navbar-social-media {
+      display: flex;
+      align-items: center;
+    }
+
+    .navbar-horizontal-links a,
+    .navbar-social-media a {
+      color: white; /* Text color for the links and icons */
+      margin-right: 100px; /* Adjust the spacing between links and icons as needed */
+    }
+
+    .social-icons {
+      display: flex;
+      align-items: center; 
+    }
+
+    .social-icon {
+      margin-right: 10px;
+      color: black;
+      font-size: 25px;
+    }
+
+    .college-name {
+      font-size: 25px;
+      line-height: 1.4;
+      color: navy;
+      text-align: center;
+      margin-bottom: 10px;
+      font-family: 'Georgia', serif; /* Add the desired font family */
+      text-transform: none;
+    }
+
+    .social-logo {
+      width: 35px; /* Adjust the width as needed */
+      height: auto; /* Maintain aspect ratio */
+      margin-left: -5px;
+    }
+
+    .nav-link {
+      margin-left: -35px;
+    }
+  </style>
+</head>
+<body>
+
+
+</body>
+</html>
